1c4b008e27293e581411a7fae9825dc4bc6bd80e
T. Lanette <TLPollard4516@gmail.com>
2020-04-21 16:55:34 -0400

    Starting again
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 
 import Container from 'react-bootstrap/Container';
 
-import {Header} from '../src/components/Header';
-import {Footer} from '../src/components/Footer';
-import DrumPad from '../src/components/DrumPad';
-//import {DrumMachine} from '../src/components/DrumMachine';
+
 
 
 
@@ -124,141 +121,11 @@ const bankTwo = [{
 ];
 
 
-class PadBank extends React.Component {
-  // eslint-disable-next-line 
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-  }
-  render() {
-    let padBank;
-    this.props.power ? padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
-      return (
-        <DrumPad clipId= {padBankArr[i].id} clip= {padBankArr[i].url} keyTrigger= {padBankArr[i].keyTrigger} keyCode= {padBankArr[i].keyCode} updateDisplay= {this.props.updateDisplay} power={this.props.power} />
-
-      )
-    }):
-    padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
-      return (
-        <DrumPad clipId= {padBankArr[i].id}
-        clip="#" keyTrigger={padBankArr[i].keyTrigger} keyCode={padBankArr[i].keyCode} updateDisplay= {this.props.updateDisplay} power= {this.props.power} />
-      )
-    });
-    return (
-      <div className="pad-bank">
-        {padBank}
-      </div>
-    )
   }
 }
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      power: true,
-      display: String.fromCharCode(160),
-      currentPadBank: bankOne, 
-      currentPadBankId: 'Heater Kit', 
-      sliderVal: 0.3
-    }
-    this.displayClipName= this.displayClipName.bind(this);
-    this.selectBank = this.selectBank.bind(this);
-    this.adjustVolume = this.adjustVolume.bind(this);
-    this.powerControl = this.powerControl.bind(this);
-    this.clearDisplay = this.clearDisplay.bind(this);
-  }
-  powerControl(){
-    this.setState({
-      power: !this.state.power,
-      display: String.fromCharCode(160)
-    }); 
-  }
-  selectBank() {
-    if (this.state.power) {
-      this.state.currentPadBankId === 'Heater Kit' ? this.setState({
-        currentPadBank: bankTwo,
-        display: 'Smooth Piano Kit',
-        currentPadBankId: 'Smooth Piano Kit',
-      }):
-      this.setState({
-        currentPadBank: bankOne,
-        display: 'Heater Kit',
-        currentPadBankId: 'Heater Kit', 
-      });
-    }
-  }
-  displayClipName(name) {
-    if (this.state.power) {
-      this.setState({
-        display: name
-      });
-    }
-  }
-  adjustVolume(e) {
-    if (this.state.power){
-      this.setState({
-        sliderVal: e.target.value,
-        display: "Volume: " + Math.round(e.target.value * 100)
-      });
-      setTimeout(() =>
-      this.clearDisplay(), 1000); 
-    }
-  }
-  clearDisplay() {
-    this.setState({
-      display: String.fromCharCode(160)
-    });
-  }
-  render(){
-    const powerSlider = this.state.power ? {
-      float: 'right'
-    } : {
-      float: 'left'
-    };
-    const bankSlider = this.state.currentPadBank === bankOne ? { float: 'left'} : {float: 'right'}; {
-      const clips = [].slice.call(document.getElementsByClassName('clip'));
-      clips.forEach(sound => {
-        sound.volume = this.state.sliderVal
-      });
-    }
 
-    return (
-      <div>
-        <Container id="drum-machine" className="inner-container">
-          <Header />
-          <div className="drum-display" id="display">
-            <h1>{this.state.display}</h1>
-          </div>
-          
-          <PadBank power={this.state.power} updateDisplay= {this.displayClipName} clipVolume= {this.state.sliderVal} currentPadBank= {this.state.currentPadBank} />
-          <div className="controls-container">
-            <div className="control">
-              <p>Power</p>
-              <div onClick= {this.powerControl} className= "select">
-                <div style= {powerSlider} className="inner"/>
-              </div>
-            </div>
-            <div className="volume-slider">
-              <input type="range" min="0" max="1" step="0.01" value= {this.state.sliderVal} onChange= {this.adjustVolume}/>
-            </div>
-            <div className="control">
-              <p>Bank</p>
-              <div onClick= {this.selectBank} className="select">
-                <div style={bankSlider} className="inner"/>
-              </div>
-            </div>
-          </div>
-          <Footer />
-        </Container>
-      </div>
-    )
-  }
-};
-
-
-
-
-
-
-export default App
