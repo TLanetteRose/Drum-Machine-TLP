a828313b30b101e5972236f920205db2c33171ac
T. Lanette <TLPollard4516@gmail.com>
2020-04-21 19:07:59 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 
 import {Header} from '../src/components/Header';
 import {Footer} from '../src/components/Footer';
+import DrumPad from '../src/components/DrumPad';
 
 
 
@@ -125,7 +126,7 @@ const bankTwo = [{
 export default class App extends Component {
   constructor(props) {
     super(props);
-    
+
 
 
 
@@ -141,5 +142,27 @@ export default class App extends Component {
   }
 }
 
+class PadBank extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let padBank; 
+    this.props.power ? padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+      return (
+        <DrumPad clipId={padBankArr[i].id} clip={padBankArr[i].url} keyTrigger={padBankArr[i].keyTrigger} keyCode={padBankArr[i].keyCode} updateDisplay={this.props.updateDisplay} power={this.props.power} />
+      )
+    }) : padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+      return (
+        <DrumPad clipId={padBankArr[i].id} clip="#" keyTrigger={padBankArr[i].keyTrigger} keyCode={padBankArr[i].keyCode} updateDisplay={this.props.updateDisplay} power={this.props.power} />
+      )
+    });
+    return (
+      <div className="pad-bank">
+        {padBank}
+      </div>
+    )
+  }
+}
 
 
