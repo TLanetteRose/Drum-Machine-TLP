e8bdaf2495ce387b8d9bf11b734dc85a7ff38ee4
T. Lanette <TLPollard4516@gmail.com>
2020-04-21 20:05:11 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.js
+++ src/App.js
@@ -126,13 +126,78 @@ const bankTwo = [{
 export default class App extends Component {
   constructor(props) {
     super(props);
-
-
-
-
+    this.state = {
+      power: true,
+      display: String.fromCharCode(160),
+      currentPadBank: bankOne,
+      currentPadBankId: 'Heater Kit',
+      sliderVal: 0.3
+    }
+    this.displayClipName = this.displayClipName.bind(this);
+    this.selectBank = this.selectBank.bind(this);
+    this.adjustVolume = this.adjustVolume.bind(this);
+    this.powerControl = this.powerControl.bind(this);
+    this.clearDisplay = this.clearDisplay.bind(this);
+  }
+  powerControl() {
+    this.setState({
+      power: !this.state.power,
+      display: String.fromCharCode(160)
+    });
+  }
+  selectBank() {
+    if (this.state.power) {
+      this.state.currentPadBankId === 'Heater Kit' ? this.setState({
+        currentPadBank: bankTwo,
+        display: 'Smooth Piano Kit', 
+        currentPadBankId: 'Smooth Piano Kit', 
+      }) : this.setState({
+        currentPadBank: bankOne,
+        display: 'Heater Kit',
+        currentPadBankId: 'Heater Kit',
+      });
+    }
+  }
+  displayClipName(name) {
+    if (this.state.power) {
+      this.setState({
+        display: name
+      });
+    }
+  }
+  adjustVolume(e){
+    if (this.state.power) {
+      this.setState({
+        sliderVal: e.target.value,
+        display: "Volume: " + Math.round(e.target.value * 100)
+      });
+      setTimeout(() => this.clearDisplay(),1000);
+    }
+  }
+  clearDisplay() {
+    this.setState({
+      display: String.fromCharCode(160)
+    });
   }
 
   render() {
+    const powerSlider = this.state.power ? {
+      float: 'right'
+    } : {
+      float: 'left'
+    };
+    const bankSlider = this.state.currentPadBank === bankOne ? {
+      float: 'left'
+    }: {
+      float: 'right'
+    }; {
+      const clips = [].slice.call(document.getElementsByClassName('clip'));
+      clips.forEach(sound => {
+        sound.volume = this.state.sliderVal
+      });
+    }
+
+
     return (
       <Container className="container">
         <Header />
@@ -143,6 +208,7 @@ export default class App extends Component {
 }
 
 class PadBank extends Component {
+  // eslint-disable-next-line 
   constructor(props) {
     super(props);
   }
